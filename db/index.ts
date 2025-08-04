a2d7d44f05d137eb7f6695a0f619cdb08698c7e6
sahaiash <sahaiashutoshvinayak@gmail.com>
2025-08-04 16:29:03 +0530

    Hosted the db on cloud

--- db/index.ts
+++ db/index.ts
@@ -1,4 +1,12 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
 
-export const db = drizzle(process.env.DATABASE_URL!);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+export const db = drizzle(pool);
